614823144879782cd5752960646a89151636b78f
nuts-rice <msis44443@gmail.com>
2022-05-07 22:28:24 -0400

    feat: lexing operators and lexemes

---
+++ src/scanner.rs
@@ -0,0 +1,167 @@
+#[derive(Debug)]
+pub enum TokenType{
+    LeftParen, RightParen, LeftBrace, RightBrace, 
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
+
+    Identifier, String, Number,
+
+    And, Class, Else, False, Fun, For, If, Nil, Or, 
+    Print, Return, Super, This, True, Var, While,
+
+    Eof
+}
+
+#[derive(Debug)]
+pub enum Literal{
+    Identifier(String),
+    Str(String),
+    Number(f64)
+}
+
+#[derive(Debug)]
+pub struct Token{
+    ty: TokenType,
+    lexeme: Vec<u8>,
+    literal: Option<Literal>,
+    line: usize,
+}
+
+pub fn scan_tokens(input: String) -> Result<Vec<Token>, String>{
+    let mut scanner : Scanner = Default::default();
+
+    scanner.scan_tokens(input);
+
+    match scanner.err {
+        Some(err) => Err(err),
+        None => Ok(scanner.tokens),
+    }   
+}
+
+struct Scanner {
+    source: Vec<u8>,
+    tokens: Vec<Token>,
+    err: Option<String>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Default for Scanner {
+    fn default() -> Scanner {
+        Scanner {
+            source: Vec::new(),
+            tokens: Vec::new(),
+            err: None,
+            start: 0,
+            current: 0,
+            line: 1
+        }
+    }
+}
+
+impl Scanner {
+    fn scan_tokens(&mut self, input: String) {
+        self.source = input.into_bytes();
+        while !self.done() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        match self.err {
+            Some(_) => {}
+            None => self.tokens.push(Token { 
+                ty: TokenType::Eof,
+                lexeme: Vec::new(),
+                literal: None,
+                line: self.line,
+            }),
+        }
+    }
+                
+            
+    fn advance(&mut self) -> char{
+        self.current += 1;
+        
+        char::from(self.source[self.current - 1 ])
+    }
+    
+    fn scan_token(&mut self) {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                let matches_eq = self.matches('=');
+                self.add_token(if matches_eq {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                })
+            }
+            '<' => {
+                let matches_eq = self.matches('=');
+                self.add_token(if matches_eq {
+                    TokenType:: GreaterEqual
+                } else {
+                    TokenType::Greater
+                })
+            }
+            c => self.err = Some(format!("scanner can't handle {}", c)) 
+        }
+        unimplemented!()
+    }
+    
+    fn add_token(&mut self, token_type: TokenType){
+        self.add_token_literal(token_type, None)
+    }
+
+    fn add_token_literal(&mut self, token_type: TokenType, literal: Option<Literal>) { 
+        let text = self.source[self.start..self.current].to_vec();
+            
+
+        self.tokens.push(Token{ ty: token_type,
+                                lexeme : text,
+                                literal,
+                                line: self.line })
+    }
+    
+//only consume character if it matches branch for next char    
+    fn matches(&mut self, c: char) -> bool {
+        if self.is_at_end() {
+            return true;
+        }
+
+        if char::from(self.source[self.current]) != c {
+            return false;
+        }   
+
+        self.current += 1;
+        true
+    }
+
+    fn done(&self) -> bool {
+        self.err.is_some() || self.is_at_end()
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+}
+
+
+
+
