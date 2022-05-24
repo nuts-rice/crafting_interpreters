faee824a08dfb5f2894cfc30e34935a8f969ff35
nuts-rice <msis44443@gmail.com>
2022-05-24 17:55:32 -0400

    fix: divide by zero

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -33,7 +33,13 @@ fn interpret_binary(lhs_expr: &expr::Expr, op: expr::BinaryOp, rhs_expr: &expr::
         (Value::Number(n1), expr::BinaryOp::Plus, Value::Number(n2)) => Ok(Value::Number(n1 + n2)),
         (Value::Number(n1), expr::BinaryOp::Minus, Value::Number(n2)) => Ok(Value::Number(n1 - n2)),
         (Value::Number(n1), expr::BinaryOp::Star, Value::Number(n2)) => Ok(Value::Number(n1 * n2)),
-        (Value::Number(n1), expr::BinaryOp::Slash, Value::Number(n2)) => Ok(Value::Number(n1 / n2)),
+        (Value::Number(n1), expr::BinaryOp::Slash, Value::Number(n2)) => {
+            if *n2 != 0.0 {
+                Ok(Value::Number(n1/n2))
+            } else {
+                Err(String::from("divide by zero"))
+            }      
+        }
         (Value::String(s1), expr::BinaryOp::Plus, Value::String(s2)) => {
             Ok(Value::String(format!("{}{}", s1, s2)))
         }
