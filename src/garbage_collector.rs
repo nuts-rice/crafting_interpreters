5c096bdadd0629b983929d7cfce52826b3c26ffa
nuts-rice <msis44443@gmail.com>
2023-01-18 13:31:03 -0500

    ⚡DRY heap allocated gc vals️

--- src/garbage_collector.rs
+++ src/garbage_collector.rs
@@ -1,8 +1,6 @@
 use crate::bytecode;
 use crate::garbage_collector_vals;
 
-use std::collections::HashMap;
-
 enum GCData {
     String(String),
     #[allow(dead_code)]
@@ -17,22 +15,8 @@ impl GCData {
         }
     }
 
-    fn as_str_mut(&mut self) -> Option<&mut String> {
-        match self {
-            GCData::String(s) => Some(s),
-            _ => None,
-        }
-    }
-
     fn as_closure(&self) -> Option<&bytecode::Closure> {
         match &self {
-            GCData::Closure(c) => Some(c),
-            _ => None,
-        }
-    }
-
-    fn as_closure_mut(&mut self) -> Option<&mut bytecode::Closure> {
-        match self {
             GCData::Closure(c) => Some(c),
             _ => None,
         }
@@ -57,60 +41,29 @@ impl GCval {
 #[derive(Default)]
 pub struct Heap {
     id_counter: garbage_collector_vals::Id,
-    values: HashMap<garbage_collector_vals::Id, GCval>,
+    values: Vec<GCval>,
 }
 
 impl Heap {
     #[allow(dead_code)]
     pub fn manage_str(&mut self, s: String) -> garbage_collector_vals::GcString {
-        let id = self.alloc_id();
-        self.values.insert(id, GCval::from(GCData::String(s)));
-        garbage_collector_vals::GcString(id)
+        self.values.push(GCval::from(GCData::String(s)));
+        garbage_collector_vals::GcString(self.values.len() - 1)
     }
 
     #[allow(dead_code)]
     pub fn manage_closure(&mut self, c: bytecode::Closure) -> garbage_collector_vals::GcClosure {
-        let id = self.alloc_id();
-        self.values.insert(id, GCval::from(GCData::Closure(c)));
-        garbage_collector_vals::GcClosure(id)
-    }
-
-    #[allow(dead_code)]
-    fn alloc_id(&mut self) -> garbage_collector_vals::Id {
-        while self.values.contains_key(&self.id_counter) {
-            self.id_counter += 1
-        }
-        self.id_counter
+        self.values.push(GCval::from(GCData::Closure(c)));
+        garbage_collector_vals::GcClosure(self.values.len() - 1)
     }
 
     #[allow(dead_code)]
     pub fn get_str(&self, s: garbage_collector_vals::GcString) -> &String {
-        self.values.get(&s.0).unwrap().data.as_str().unwrap()
+        self.values[s.0].data.as_str().unwrap()
     }
 
     #[allow(dead_code)]
-    pub fn get_str_mut(&mut self, s: garbage_collector_vals::GcString) -> &mut String {
-        self.values
-            .get_mut(&s.0)
-            .unwrap()
-            .data
-            .as_str_mut()
-            .unwrap()
-    }
-    #[allow(dead_code)]
     pub fn get_closure(&self, c: garbage_collector_vals::GcClosure) -> &bytecode::Closure {
-        self.values.get(&c.0).unwrap().data.as_closure().unwrap()
-    }
-    #[allow(dead_code)]
-    pub fn get_closure_mut(
-        &mut self,
-        c: garbage_collector_vals::GcClosure,
-    ) -> &mut bytecode::Closure {
-        self.values
-            .get_mut(&c.0)
-            .unwrap()
-            .data
-            .as_closure_mut()
-            .unwrap()
+        self.values[c.0].data.as_closure().unwrap()
     }
 }
