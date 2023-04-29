e19e9045b68dd38edc092aba8743709b17355763
nuts-rice <red44443@gmail.com>
2023-04-29 17:49:20 -0400

    🚧 prototyping gc

--- src/garbage_collector.rs
+++ src/garbage_collector.rs
@@ -1,4 +1,6 @@
-use crate::bytecode;
+use std::collections::HashMap;
+
+use crate::{bytecode};
 
 enum GCData {
     String(String),
@@ -36,41 +38,88 @@ impl GCval {
         }
     }
 }
-#[derive(Default)]
+
 pub struct Heap {
     bytes_allocated: usize,
+    id_counter: usize,
     next_gc: usize,
-    values: Vec<GCval>,
+    values: HashMap<usize, GCval>,
+}
+
+impl Default for Heap {
+    fn default() -> Heap {
+        let next_gc = std::env::var("LOX_GC_TRIGGER_SIZE")
+            .ok()
+            .and_then(|env_str| env_str.parse::<usize>().ok())
+            .unwrap_or(1024 * 1024);
+        Heap {
+            bytes_allocated: 0,
+            id_counter: 0,
+            next_gc,
+            values: Default::default(),
+        }
+    }
 }
 
 impl Heap {
-    #[allow(dead_code)]
+    fn generate_id(&mut self) -> usize {
+        self.id_counter += 1;
+        loop {
+            if !self.values.contains_key(&self.id_counter) {
+                return self.id_counter;
+            }
+            self.id_counter += 1;
+        }
+    }
     pub fn manage_str(&mut self, s: String) -> usize {
-        self.values.push(GCval::from(GCData::String(s)));
-        self.values.len() - 1
+        self.bytes_allocated += s.len();
+        let id = self.generate_id();
+        self.values.insert(id, GCval::from(GCData::String(s)));
+        id
     }
 
-    #[allow(dead_code)]
     pub fn manage_closure(&mut self, c: bytecode::Closure) -> usize {
-        self.values.push(GCval::from(GCData::Closure(c)));
-        self.values.len() - 1
+        self.bytes_allocated += c.function.chunk.code.len();
+        self.bytes_allocated += c.function.chunk.constants.len();
+        let id = self.generate_id();
+        self.values.insert(id, GCval::from(GCData::Closure(c)));
+        id
     }
 
-    #[allow(dead_code)]
     pub fn get_str(&self, id: usize) -> &String {
-        self.values[id].data.as_str().unwrap()
+        self.values.get(&id).unwrap().data.as_str().unwrap()
     }
 
-    #[allow(dead_code)]
     pub fn get_closure(&self, id: usize) -> &bytecode::Closure {
-        self.values[id].data.as_closure().unwrap()
+        self.values.get(&id).unwrap().data.as_closure().unwrap()
+    }
+
+    pub fn unmark(&mut self) {
+        for val in self.values.values_mut() {
+            val.is_marked = false;
+        }
     }
 
     pub fn mark(&mut self, id: usize) {
-        self.values[id].is_marked = true;
+        self.values.get_mut(&id).unwrap().is_marked = true;
     }
 
-    pub fn mark_closure(&mut self, id: usize) {
-        self.values[id].is_marked = true;
+    pub fn is_marked(&self, id: usize) -> bool {
+        self.values.get(&id).unwrap().is_marked
+    }
+
+    pub fn children(&self, _id:usize) -> Vec<usize> {
+        todo!()
+    }
+
+    pub fn closure_children(&self, _closure: &bytecode::Closure) -> Vec<usize> {
+        todo!()
+    }
+    pub fn sweep(&mut self) {
+        todo!()
+    }
+
+    pub fn collect(&self) -> bool {
+        todo!()
     }
 }
